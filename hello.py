d50908ffa27b22be2a384bc17164de35fa6809b1
jyothi <jyothi@gmail.com>
2022-07-05 23:08:57 +0530

    first hello

---
+++ hello.py
@@ -0,0 +1,10 @@
+https://github.com/jyothijyo1501/dakshan.gitMedia contacts
+Subscribe to the media mailing list ►
+
+Media Team email: [ Click to reveal ]
+
+Mr. Ismaila Dieng
+Chief, Communications and Advocacy
+Email: [ Click to reveal ]
+
+Ms. Mandy Kibel
